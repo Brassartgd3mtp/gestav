5908b2fa5fc1a0b76bf0c229a754db4c0f00964a
RemDrz <remiroucairol3221@gmail.com>
2023-10-16 09:52:34 +0200

    Modification pour permettre à plusieurs ouvriers de se d'aplcer sur une même destination sans créer de problèmes de collisions

--- Assets/Scripts/Units/Characters/CharacterManager.cs
+++ Assets/Scripts/Units/Characters/CharacterManager.cs
@@ -6,7 +6,8 @@ public class CharacterManager : UnitManager
 {
     private Character character;
     [SerializeField] private NavMeshAgent agent;
-
+    [SerializeField] private float stoppingDistance;
+    [SerializeField] private float stoppingMultiplicator = 0.3f;
     protected override Unit Unit
     {
         get { return character; }
@@ -15,7 +16,22 @@ public class CharacterManager : UnitManager
 
     public void MoveTo(Vector3 targetPosition)
     {
+        stoppingDistance = stoppingMultiplicator * (Global.SELECTED_UNITS.Count -1f);
+        // Stop the current movement
+        agent.isStopped = true;
+        if(Global.SELECTED_UNITS.Count > 1)
+        {
+
+            Vector3 randomOffset = new Vector3( Random.Range(-stoppingDistance, stoppingDistance),0,Random.Range(-stoppingDistance, stoppingDistance));
+
+            agent.destination = targetPosition + randomOffset;
+        }
+        else
+        // Set the new destination
         agent.destination = targetPosition;
+
+        // Resume movement
+        agent.isStopped = false;
     }
 
 }
